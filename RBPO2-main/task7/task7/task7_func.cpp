ea5bb744b98b1ca60014967882d393e8e8384fb1
Fynjy1002 <90138028+Fynjy1002@users.noreply.github.com>
2022-01-20 21:28:23 +0300

    Update task7_func.cpp

--- RBPO2-main/task7/task7/task7_func.cpp
+++ RBPO2-main/task7/task7/task7_func.cpp
@@ -1,9 +1,9 @@
-﻿#include <cmath>
+#include <cmath>
 
-static double x = 5;
-static double y = 6;
-static double res;
+double x ;
+double y;
+double res;
 
-static void f() {
+void f() {
     res = pow(cos(x), 4) + pow(sin(y), 2) + 1 / 4 * pow(sin(2 * x), 2) - 1;
 }
